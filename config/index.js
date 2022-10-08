a7edce26500e60239bf5b93b0a7f3560b58c9275
Semeton Balogun <balogunsemeton@gmail.com>
2022-10-08 20:45:15 +0100

    db config

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    live_url:
+      "mongodb+srv://Semeton:Midonugbo_Tadegbe97@plexxos.eptrvht.mongodb.net/plexxosdb?retryWrites=true&w=majority",
+    local_url: "localhost:27017/plexxosdb",
+    // name: "plexxosdb",
+  },
+};
+
+module.exports = config;
---
+++ config/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = "mongoose";
+const config = require("./index.js");
+
+const LOCAL_CONNECTION_URL = `mongodb://${config.db.local_url}`;
+const LIVE_CONNECTION_URL = `${config.db.live_url}`;
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongo has connected succesfully");
+});
+mongoose.connection.on("reconnected", () => {
+  console.log("Mongo has reconnected");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("Mongo connection has an error", error);
+  mongoose.disconnect();
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongo connection is disconnected");
+});
