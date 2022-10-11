70dc4d658b77b1b5d4037c1e984df98cc6474366
Semeton Balogun <balogunsemeton@gmail.com>
2022-10-11 23:05:05 +0100

    Get a user by id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,8 +2,22 @@ import makeValidation from "@withvoid/make-validation";
 import UserModel, { USER_TYPES } from "../models/User.js";
 
 export default {
-  onGetAllUsers: async (req, res) => {},
-  onGetUserById: async (req, res) => {},
+  onGetAllUsers: async (req, res) => {
+    try {
+      const users = await UserModel.getUsers();
+      return res.status(200).json({ success: true, users });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
+  onGetUserById: async (req, res) => {
+    try {
+      const user = await UserModel.getUserById(req.params.id);
+      return res.status(200).json({ success: true, user });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
   onCreateUser: async (req, res) => {
     try {
       const validation = makeValidation((types) => ({
@@ -25,7 +39,17 @@ export default {
       return res.status(500).json({ success: false, error: error });
     }
   },
-  onDeleteUserById: async (req, res) => {},
+  onDeleteUserById: async (req, res) => {
+    try {
+      const user = await UserModel.deleteByUserById(req.params.id);
+      return res.status(200).json({
+        success: true,
+        message: `Deleted a count of ${user.deletedCount} user.`,
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
 };
 
 // Really
