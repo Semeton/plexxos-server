361788d5af1490e96b7bad3acaff1d1a13e7523a
Semeton Balogun <balogunsemeton@gmail.com>
2022-10-11 23:05:57 +0100

    delete a user by ID

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,16 @@ import { encode } from "../middlewares/jwt.js";
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  res
+    .status(200)
+    .json({ status: true, message: "Plexxos root server is running" });
+});
+
 router.get("/api", (req, res, next) => {
-  res.status(200).json({ status: true, message: "Plexxos server is running" });
+  res
+    .status(200)
+    .json({ status: true, message: "Plexxos api server is running" });
 });
 
 router.post("/login/:userId", encode, (req, res, next) => {});
