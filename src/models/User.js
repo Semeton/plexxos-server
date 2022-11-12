2f00f33f40c454a86dae9f360a241fc294b0fa0f
Semeton <balogunsemeton@gmail.com>
2022-11-12 12:04:28 +0100

    new changes

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,10 @@
 import { json } from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 import FacilityModel from "./Facility.js";
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema(
   {
@@ -76,3 +79,80 @@ userSchema.statics.deleteByUserById = async function (id) {
 };
 
 export default mongoose.model("User", userSchema);
+
+//hash password in mongoose?
+// var mongoose = require("mongoose"),
+//   Schema = mongoose.Schema,
+//   bcrypt = require("bcrypt"),
+//   SALT_WORK_FACTOR = 10;
+
+// var UserSchema = new Schema({
+//   username: { type: String, required: true, index: { unique: true } },
+//   password: { type: String, required: true },
+// });
+
+// UserSchema.pre("save", function (next) {
+//   var user = this;
+
+//   // only hash the password if it has been modified (or is new)
+//   if (!user.isModified("password")) return next();
+
+//   // generate a salt
+//   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+//     if (err) return next(err);
+
+//     // hash the password using our new salt
+//     bcrypt.hash(user.password, salt, function (err, hash) {
+//       if (err) return next(err);
+//       // override the cleartext password with the hashed one
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
+
+// UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// };
+
+// module.exports = mongoose.model("User", UserSchema);
+
+// var mongoose = require(mongoose),
+//   User = require("./user-model");
+
+// var connStr = "mongodb://localhost:27017/mongoose-bcrypt-test";
+// mongoose.connect(connStr, function (err) {
+//   if (err) throw err;
+//   console.log("Successfully connected to MongoDB");
+// });
+
+// // create a user a new user
+// var testUser = new User({
+//   username: "jmar777",
+//   password: "Password123",
+// });
+
+// // save the user to database
+// testUser.save(function (err) {
+//   if (err) throw err;
+// });
+
+// // fetch the user and test password verification
+// User.findOne({ username: "jmar777" }, function (err, user) {
+//   if (err) throw err;
+
+//   // test a matching password
+//   user.comparePassword("Password123", function (err, isMatch) {
+//     if (err) throw err;
+//     console.log("Password123:", isMatch); // -&gt; Password123: true
+//   });
+
+//   // test a failing password
+//   user.comparePassword("123Password", function (err, isMatch) {
+//     if (err) throw err;
+//     console.log("123Password:", isMatch); // -&gt; 123Password: false
+//   });
+// });
